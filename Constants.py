8886a5e2f0614ec39fca2d1ee9d1be14d39a65c1
remilvus <remilvus@gmail.com>
2020-09-09 23:01:38 +0200

    Added time related constants

--- Constants.py
+++ Constants.py
@@ -4,7 +4,10 @@ TRAM_INFO_FOLDER = 'GTFS_KRK_T'
 BUS_POSITIONS_PATH = 'VehiclePositions_A'
 TRAM_POSITIONS_PATH = 'VehiclePositions_T'
 SEC_IN_DAY = 24 * 60 * 60
-MIN_STOP_NUMBER = 5
+PLOT_TIME_STEP = 30 * 60  # specifies resolution of x axis for plots with general delay statistics
+MIN_STOP_NUMBER = 3  # todo: set to length of shortest route
+MAX_BUS_INACTIVITY = 20 * 60  # max time (in seconds) a trip can exist without any update
+
 
 # gtfs-realtime VehicleStopStatus
 INCOMING_AT = 0
