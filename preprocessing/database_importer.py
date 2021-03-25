b6b08d1a342b7ad7ad2dd8de079a64651f8db979
Gazda70 <pjgazda@interia.pl>
2021-03-25 22:11:13 +0100

    Basic code organisation for database connection.

--- preprocessing/database_importer.py
+++ preprocessing/database_importer.py
@@ -3,8 +3,15 @@ import sqlite3
 from utilities.constants import RAW_DATA_PATH, DATABASE_PATH, ALERTS_DB_PATH, \
     GTFS_DB_PATH, UPDATES_DB_PATH, POSITIONS_DB_PATH
 
-if not DATABASE_PATH.exists():
-    DATABASE_PATH.mkdir()
+class Connection:
+    def __init__(self):
+        if not DATABASE_PATH.exists():
+            DATABASE_PATH.mkdir()
+        self.con = sqlite3.connect(str(DATABASE_PATH.joinpath(UPDATES_DB_PATH)))
 
-con = sqlite3.connect(DATABASE_PATH.joinpath(UPDATES_DB_PATH))
-con.close()
+    def close(self):
+        self.con.close()
+
+
+connection = Connection()
+connection.close()
