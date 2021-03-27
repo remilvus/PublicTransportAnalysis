44260eeb241b783f7ef13fc597ecf1a04eed5fe9
Gazda70 <pjgazda@interia.pl>
2021-03-27 15:39:53 +0100

    Create and insert faciliation

--- preprocessing/database_importer.py
+++ preprocessing/database_importer.py
@@ -12,6 +12,34 @@ class Connection:
     def close(self):
         self.con.close()
 
+    def create_table(self, tableName, myColList):
+        sqlString = "CREATE TABLE " + tableName + " ("
+        for colIdx in range(0, len(myColList)):
+            sqlString += myColList[colIdx]
+            if(colIdx != len(myColList) - 1):
+                sqlString += ", "
+        sqlString += ");"
+        #self.con.execute(self, sqlString)
+        return sqlString
+
+    def insert_to_table(self, tableName, nameValuePairs):
+        sqlString = "INSERT INTO " + tableName + " ("
+        insertValues = " VALUES ("
+        for nVP in range(0, len(nameValuePairs)):
+            sqlString += nameValuePairs[nVP][0]
+            insertValues += nameValuePairs[nVP][1]
+            if(nVP != len(nameValuePairs)-1):
+                sqlString += ", "
+                insertValues += ", "
+        sqlString += ")"
+        insertValues += ");"
+        sqlString += insertValues
+        #self.con.execute(self, sqlString)
+        return sqlString
 
 connection = Connection()
+toCreate = ["ID INT PRIMARY KEY NOT NULL", "SOMEPHRASE CHAR(50)"]
+toInsert = [["ID", "1"], ["NAME", "PIOTR"]]
+print(connection.create_table("MYTABLE", toCreate))
+print(connection.insert_to_table("PEOPLE", toInsert))
 connection.close()
